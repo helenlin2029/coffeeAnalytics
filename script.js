61a2480e98b8a38420eb9eda5f5d9ede3707e851
Helen Lin <linhelentrade@gmail.com>
2026-03-25 18:36:01 -0700

    added functions for buttons to enable dynamic insertion

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Logic to switch screens
 function showView(viewName) {
     const customerDiv = document.getElementById('customer-view');
@@ -13,7 +12,8 @@ function showView(viewName) {
     }
 }
 
-// This array acts as our temporary database
+
+
 let orders = [];
 function showView(viewName) {
     const customerView = document.getElementById('customer-view');
@@ -29,6 +29,8 @@ function showView(viewName) {
     }
 }
 
+
+
 function goToHome() {
     const home = document.getElementById('customer-view');
     const analytics = document.getElementById('owner-view');
@@ -36,3 +38,30 @@ function goToHome() {
     home.style.display = 'block';
     analytics.style.display = 'none';
 }
+
+
+
+const buttons = document.querySelectorAll('.menu-item');
+const orderList = document.querySelector('.order-list');
+const totalDisplay = document.querySelector('.total-row span:last-child');
+
+let currentTotal = 0;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const name = button.getAttribute('item-name');
+        const price = parseFloat(button.getAttribute('item-price'));
+
+        const newItem = document.createElement('div');
+        newItem.classList.add('order-item');
+        newItem.innerHTML = `
+            <span>${name}</span>
+            <span>$${price.toFixed(2)}</span>
+        `;
+
+        orderList.appendChild(newItem);
+
+        currentTotal += price;
+        totalDisplay.innerText = `$${currentTotal.toFixed(2)}`;
+    });
+});
